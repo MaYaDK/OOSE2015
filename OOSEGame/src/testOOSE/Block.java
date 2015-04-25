6aa2a7d13d631b14abdbdadb02cf4e469d4eee53
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-04-25 22:51:03 +0200

    Collision Ball/Block: I created collision detection for each block, so each block now disappear when its hit by the ball.

--- OOSEGame/src/testOOSE/Block.java
+++ OOSEGame/src/testOOSE/Block.java
@@ -5,18 +5,62 @@ import java.awt.Graphics;
 
 public class Block 
 {
-	int blockPlaceX = 0, blockPlaceY = 10, blockwidth = 100, blockheight = 30;
-
-	public void drawBlock(Graphics g)
+	public int blockPlaceX1 = 0, blockPlaceY1 = 10, blockwidth = 100, blockheight = 30;
+	public int blockPlaceX2 = blockPlaceX1+125, blockPlaceY2 = 10;
+	public int blockPlaceX3 = blockPlaceX1+125*2, blockPlaceY3 = 10;
+	public int blockPlaceX4 = blockPlaceX1+125*3, blockPlaceY4 = 10;
+	public int blockPlaceX5 = blockPlaceX1+125*4, blockPlaceY5 = 10;
+	
+	public int blockPlaceX1_2 = 0, blockPlaceY1_2 = 60;
+	public int blockPlaceX2_2 = blockPlaceX1+125, blockPlaceY2_2 = 60;
+	public int blockPlaceX3_2 = blockPlaceX1+125*2, blockPlaceY3_2 = 60;
+	public int blockPlaceX4_2 = blockPlaceX1+125*3, blockPlaceY4_2 = 60;
+	public int blockPlaceX5_2 = blockPlaceX1+125*4, blockPlaceY5_2 = 60;
+	
+	//Row 1
+	public void drawBlock1(Graphics g)
 	{
-		//Draw blocks
-	    for( blockPlaceX = 0; blockPlaceX < 640; blockPlaceX = blockPlaceX+blockwidth+20) {
-	        for(blockPlaceY = 0; blockPlaceY < 250; blockPlaceY=blockPlaceY+blockheight+20){
-	            g.drawRect (blockPlaceX, blockPlaceY, blockwidth, blockheight);
-	            g.setColor(Color.BLACK);
-	            g.fillRect(blockPlaceX, blockPlaceY, blockwidth, blockheight);
-	            }  
-	    	}	
+		g.setColor(Color.BLACK);
+		g.fillRect(blockPlaceX1, blockPlaceY1, blockwidth, blockheight);          
+	}
+	public void drawBlock2(Graphics g)
+	{
+		g.fillRect(blockPlaceX2, blockPlaceY2, blockwidth, blockheight);
+	}
+	public void drawBlock3(Graphics g)
+	{
+		g.fillRect(blockPlaceX3, blockPlaceY3, blockwidth, blockheight);
+	}
+	public void drawBlock4(Graphics g)
+	{
+		g.fillRect(blockPlaceX4, blockPlaceY4, blockwidth, blockheight);
+	}
+	public void drawBlock5(Graphics g)
+	{
+		g.fillRect(blockPlaceX5, blockPlaceY5, blockwidth, blockheight);
+	}
+    
+    //Row 2
+	public void drawBlock6(Graphics g)
+	{
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(blockPlaceX1_2, blockPlaceY1_2, blockwidth, blockheight);
+	}
+	public void drawBlock7(Graphics g)
+	{
+		g.fillRect(blockPlaceX2_2, blockPlaceY2_2, blockwidth, blockheight);
+	}
+	public void drawBlock8(Graphics g)
+	{
+		g.fillRect(blockPlaceX3_2, blockPlaceY3_2, blockwidth, blockheight);
+	}
+	public void drawBlock9(Graphics g)
+	{
+		g.fillRect(blockPlaceX4_2, blockPlaceY4_2, blockwidth, blockheight);
+	}
+	public void drawBlock10(Graphics g)
+	{
+		g.fillRect(blockPlaceX5_2, blockPlaceY5_2, blockwidth, blockheight);
 	}
 	public static void main(String[] args) 
 	{
--- OOSEGame/src/testOOSE/SimpleSlickGame.java
+++ OOSEGame/src/testOOSE/SimpleSlickGame.java
@@ -22,6 +22,18 @@ public class SimpleSlickGame extends JPanel implements ActionListener, KeyListen
 	Sound s = new Sound();
 	Timer tm = new Timer(5, this); //For animation
 	
+	boolean is1Hit = false;
+	boolean is2Hit = false;
+	boolean is3Hit = false;
+	boolean is4Hit = false;
+	boolean is5Hit = false;
+	
+	boolean is6Hit = false;
+	boolean is7Hit = false;
+	boolean is8Hit = false;
+	boolean is9Hit = false;
+	boolean is10Hit = false;
+	
 	public SimpleSlickGame()
 	{
 		//Declaring the classes.
@@ -38,9 +50,41 @@ public class SimpleSlickGame extends JPanel implements ActionListener, KeyListen
 	{
 		super.paintComponent(g);
 		b.drawBall(g); //Receiving the draw Ball method from Ball class.
-		bl.drawBlock(g);
 		p.drawPlayer(g);
-        }
+		
+		//Draw each as long as it is not hit.
+		if(is1Hit == false){
+			bl.drawBlock1(g);
+		}
+		if(is2Hit == false){
+			bl.drawBlock2(g);
+		}
+		if(is3Hit == false){
+			bl.drawBlock3(g);
+		}
+		if(is4Hit == false){
+			bl.drawBlock4(g);
+		}
+		if(is5Hit == false){
+			bl.drawBlock5(g);
+		}
+		//Row 2
+		if(is6Hit == false){
+			bl.drawBlock6(g);
+		}
+		if(is7Hit == false){
+			bl.drawBlock7(g);
+		}
+		if(is8Hit == false){
+			bl.drawBlock8(g);
+		}
+		if(is9Hit == false){
+			bl.drawBlock9(g);
+		}
+		if(is10Hit == false){
+			bl.drawBlock10(g);
+		}
+	}
 
 	public void actionPerformed(ActionEvent e)
 	{
@@ -48,14 +92,48 @@ public class SimpleSlickGame extends JPanel implements ActionListener, KeyListen
         b.collisionScreen(); //Receiving the collision with screen method from Ball class.
         p.movePlayer();
         collisionPlayerBall();
+        collisionBlockBall();
 		repaint();
 	}
 	
 	public void collisionPlayerBall(){
-        if(b.yBall > p.yRect && b.yBall < p.yRect + p.rectHeight && b.xBall > p.xRect && b.xBall < p.xRect + p.rectWidth){ //if there is the ball is within the players position.
+        if(b.xBall > p.xRect && b.xBall < p.xRect + p.rectWidth && b.yBall > p.yRect && b.yBall < p.yRect + p.rectHeight){ //if there is the ball is within the players position.
 			System.out.println("Collision Player/Ball!"); //Debug
 			b.ballVelY = b.ballVelY*-1;
 			s.playerSound();
+    	}
+	}
+	public void collisionBlockBall(){
+        if(b.xBall > bl.blockPlaceX1 && b.xBall < bl.blockPlaceX1 + bl.blockwidth && b.yBall > bl.blockPlaceY1 && b.yBall < bl.blockPlaceY1 + bl.blockheight){ //if there is the ball is within the players position.
+			is1Hit = true;
+    	}
+        if(b.xBall > bl.blockPlaceX2 && b.xBall < bl.blockPlaceX2 + bl.blockwidth && b.yBall > bl.blockPlaceY2 && b.yBall < bl.blockPlaceY2 + bl.blockheight){ //if there is the ball is within the players position.
+        	is2Hit = true;
+    	}
+        if(b.xBall > bl.blockPlaceX3 && b.xBall < bl.blockPlaceX3 + bl.blockwidth && b.yBall > bl.blockPlaceY3 && b.yBall < bl.blockPlaceY3 + bl.blockheight){ //if there is the ball is within the players position.
+        	is3Hit = true;
+    	}
+        if(b.xBall > bl.blockPlaceX4 && b.xBall < bl.blockPlaceX4 + bl.blockwidth && b.yBall > bl.blockPlaceY4 && b.yBall < bl.blockPlaceY4 + bl.blockheight){ //if there is the ball is within the players position.
+        	is4Hit = true;
+    	}
+        if(b.xBall > bl.blockPlaceX5 && b.xBall < bl.blockPlaceX5 + bl.blockwidth && b.yBall > bl.blockPlaceY5 && b.yBall < bl.blockPlaceY5 + bl.blockheight){ //if there is the ball is within the players position.
+        	is5Hit = true;
+    	}
+      //Row 2
+        if(b.xBall > bl.blockPlaceX1_2 && b.xBall < bl.blockPlaceX1_2 + bl.blockwidth && b.yBall > bl.blockPlaceY1_2 && b.yBall < bl.blockPlaceY1_2 + bl.blockheight){ //if there is the ball is within the players position.
+        	is6Hit = true;
+    	}
+        if(b.xBall > bl.blockPlaceX2_2 && b.xBall < bl.blockPlaceX2_2 + bl.blockwidth && b.yBall > bl.blockPlaceY2_2 && b.yBall < bl.blockPlaceY2_2 + bl.blockheight){ //if there is the ball is within the players position.
+        	is7Hit = true;
+    	}
+        if(b.xBall > bl.blockPlaceX3_2 && b.xBall < bl.blockPlaceX3_2 + bl.blockwidth && b.yBall > bl.blockPlaceY3_2 && b.yBall < bl.blockPlaceY3_2 + bl.blockheight){ //if there is the ball is within the players position.
+        	is8Hit = true;
+    	}
+        if(b.xBall > bl.blockPlaceX4_2 && b.xBall < bl.blockPlaceX4_2 + bl.blockwidth && b.yBall > bl.blockPlaceY4_2 && b.yBall < bl.blockPlaceY4_2 + bl.blockheight){ //if there is the ball is within the players position.
+        	is9Hit = true;
+    	}
+        if(b.xBall > bl.blockPlaceX5_2 && b.xBall < bl.blockPlaceX5_2 + bl.blockwidth && b.yBall > bl.blockPlaceY5_2 && b.yBall < bl.blockPlaceY5_2 + bl.blockheight){ //if there is the ball is within the players position.
+        	is10Hit = true;
     	}
 	}
 	
